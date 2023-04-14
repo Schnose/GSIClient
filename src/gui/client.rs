e76783d89a8d3b2434670bd598cce57faf8f7d85
AlphaKeks <alphakeks@dawn.sh>
2023-04-14 05:42:58 +0200

    feat: add button to save logs to a file

--- src/gui/client.rs
+++ src/gui/client.rs
@@ -5,6 +5,7 @@ use {
 		config::Config,
 		logger::{Log, LogReceiver},
 	},
+	chrono::Utc,
 	eframe::{
 		egui::{
 			style::Selection, FontData, FontDefinitions, RichText, ScrollArea, Style, TextStyle,
@@ -14,8 +15,10 @@ use {
 		CreationContext,
 	},
 	eframe::{HardwareAcceleration, NativeOptions, Theme},
-	std::{collections::BTreeMap, sync::Arc},
+	rfd::FileDialog,
+	std::{collections::BTreeMap, fs::File, sync::Arc},
 	tokio::{sync::Mutex, task::JoinHandle},
+	tracing::{error, info, warn},
 };
 
 pub struct Client {
@@ -137,6 +140,15 @@ impl Client {
 	}
 
 	pub fn render_logs(&mut self, ui: &mut Ui) {
+		if self.logger.is_some() {
+			// TODO: put this below the scroll area
+			self.save_logs(ui);
+
+			ui.add_space(8.0);
+			ui.separator();
+			ui.add_space(8.0);
+		}
+
 		let Some(logger) = &mut self.logger else {
 			ui.vertical_centered(|ui| ui.colored_label(colors::RED, "Logs are displayed on STDOUT."));
 			return;
@@ -192,5 +204,43 @@ impl Client {
 		.heading();
 
 		ui.label(status);
+	}
+
+	pub fn save_logs(&mut self, ui: &mut Ui) {
+		use std::io::Write;
+
+		if !ui
+			.button(RichText::new("Save logs").color(colors::PEACH))
+			.clicked()
+		{
+			return;
+		}
+
+		let timestamp = Utc::now().format("%Y%m%d%H%M%S");
+		let file_name = format!("{timestamp}-schnose-gsi-client.log");
+
+		let Some(log_path) = FileDialog::new().set_file_name(&file_name).save_file() else {
+			return;
+		};
+
+		let mut file = match File::create(&log_path) {
+			Ok(file) => file,
+			Err(why) => return error!("Failed to create log file: {why:#?}"),
+		};
+
+		let Some(logger) = &mut self.logger else {
+			return error!("This UI should only be rendered if a logger is present.");
+		};
+
+		let Some(logs) = logger.current() else {
+			return warn!("Cannot save empty logs.");
+		};
+
+		let log_path = log_path.display();
+
+		match file.write_all(&logs) {
+			Ok(()) => info!("Wrote logs to `{log_path}`."),
+			Err(why) => error!("Failed to write logs to `{log_path}`: {why:#?}"),
+		}
 	}
 }
